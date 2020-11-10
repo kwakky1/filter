f6d65c3ff72ee574c840821b7e61857a240d6368
곽경열 <62980973+kwakky1@users.noreply.github.com>
2020-11-10 14:12:42 +0900

    Click 메소드 구현 후 Ture 일때 필터 완료 false 일때 작업 해야함

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,33 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MaterialTable from "material-table";
 import user from "../user.json"
 
-
-const columns = [
-    {
-        title: "아이디", field: "userId"
-    },
-    {
-        title: "이름", field: "name"
-    },
-    {
-        title: "나이", field: "age"
-    },
-    {
-        title: "성별", field: "sex"
-    },
-]
-
-const data = user
-
-
 const Table = () => {
+
+    const [userList, setUserList] = useState(user);
+
+    const columns = [
+        {
+            title: "아이디", field: "userId"
+        },
+        {
+            title: "이름", field: "name"
+        },
+        {
+            title: "나이", field: "age"
+        },
+        {
+            title: "성별", field: "sex"
+        },
+    ];
+
+    const onClick = (e) => {
+        const {target: {name, checked}} = e;
+
+        if(name === "male" && checked){
+            let maleUserList = userList.filter((element)=>{
+                return element.sex === name
+            })
+            setUserList(maleUserList)
+
+        } else if(name==="female" && checked) {
+            let maleUserList = userList.filter((element)=>{
+                return element.sex === name
+            })
+            setUserList(maleUserList)
+        }
+    }
+
     return (
         <>
+            <form action="">
+            <label>남자</label>
+            <input type="checkbox" name={"male"} onClick={onClick} />
+            <label>여자</label>
+            <input type="checkbox" name={"female"} onClick={onClick} />
+            </form>
             <MaterialTable
                 title={"회원정보"}
                 columns={columns}
-                data={data}
+                data={userList}
             />
             
         </>
